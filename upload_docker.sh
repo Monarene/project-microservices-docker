41e555c5e812691b8251bcc5524a146128de2c6c
Michael Mekuleyi <mekuleyimichael@gmail.com>
2022-09-17 03:54:46 +0100

    add circle ci

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="monarene/prediction-api"
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,5 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker tag prediction-api $dockerpath
+docker push $dockerpath
