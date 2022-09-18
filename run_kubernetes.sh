bf806a86a756de40a9d44ab81ce6df9ba96bb991
Michael Mekuleyi <mekuleyimichael@gmail.com>
2022-09-18 10:35:04 +0100

    kubernetes build successful

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,14 +4,12 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="monarene/prediction-api"
+dockerpath=monarene/prediction-api:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run prediction-api\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=prediction-api
+kubectl run prediction-api --image=$dockerpath --port=80 --labels app=prediction-api
+
 
 # Step 3:
 # List kubernetes pods
